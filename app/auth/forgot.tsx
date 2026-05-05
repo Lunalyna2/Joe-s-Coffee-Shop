aa3552450c1c3837a1d57c6ca2ad5fb5d29c9d85
Lunalyna2 <ynachloe.venegas-24@cpu.edu.ph>
2026-05-05 23:31:06 +0800

    forget pass

--- app/auth/forgot.tsx
+++ app/auth/forgot.tsx
@@ -12,7 +12,7 @@ export default function ForgotPassword() {
     e.preventDefault(); 
     //calls Supabase to send a reset password email
     const { error } = await supabase.auth.resetPasswordForEmail(email, {
-      redirectTo: `${window.location.origin}/account/updatepass`,
+    redirectTo: `${window.location.origin}/auth/confirm`,
     });
     if (error) {
       alert(error.message); 
