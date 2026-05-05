d20a722762d040442215a433164c9102194cc910
Lunalyna2 <ynachloe.venegas-24@cpu.edu.ph>
2026-05-05 18:07:35 +0800

    feat: changed routing

--- app/auth/forgot.tsx
+++ app/auth/forgot.tsx
@@ -12,7 +12,7 @@ export default function ForgotPassword() {
     e.preventDefault(); 
     //calls Supabase to send a reset password email
     const { error } = await supabase.auth.resetPasswordForEmail(email, {
-      redirectTo: "http://localhost:3000/auth/reset", 
+      redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/reset`, 
     });
     if (error) {
       alert(error.message); 
