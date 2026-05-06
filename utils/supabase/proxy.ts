6789f6f1f7698c43b22fe46823ad37eddee33744
Lunalyna2 <ynachloe.venegas-24@cpu.edu.ph>
2026-05-06 11:17:30 +0800

    fix: fixed navs

--- utils/supabase/proxy.ts
+++ utils/supabase/proxy.ts
@@ -48,8 +48,8 @@ export async function updateSession(request: NextRequest) {
   !user &&
   !request.nextUrl.pathname.startsWith("/login") &&
   !request.nextUrl.pathname.startsWith("/auth") &&
-  !request.nextUrl.pathname.startsWith("/forgot-password") && // Add this
-  !request.nextUrl.pathname.startsWith("/account/updatepass") // Add this
+  !request.nextUrl.pathname.startsWith("/auth/confirm") &&
+  !request.nextUrl.pathname.startsWith("/account/updatepass") 
 ) {
   const url = request.nextUrl.clone();
   url.pathname = "/login";
